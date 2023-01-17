66852aecfc5ada6c13db0f52c78ce9d28a47c580
Ilyaz22 <so.ilia@yandex.ru>
2023-01-17 03:28:48 +0300

    initial users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "events#index"
 
+  resources :users, only: [:show, :edit, :update]
   resources :events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_002344) do
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "address"
+    t.datetime "datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "events", "users"
 end
