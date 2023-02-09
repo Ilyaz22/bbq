97a08858bed0606123cabc6728447e183c0cd083
Ilyaz22 <so.ilia@yandex.ru>
2023-02-10 01:18:55 +0300

    add fix

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -5,14 +5,14 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = subscription.event
 
-    mail to: event.user.email, subject: "#{I18n.t("event_mailer.new_subscription")} #{event.title}"
+    mail to: @event.user.email, subject: "#{I18n.t("event_mailer.new_subscription")} #{event.title}"
   end
 
   def comment(comment, email)
     @comment = comment
     @event = comment.event
 
-    mail to: email, subject: "#{I18n.t("event_mailer.new_comment")} #{event.title}"
+    mail to: email, subject: "#{I18n.t("event_mailer.new_comment")} #{@event.title}"
   end
 
   def photo(photo, email)
