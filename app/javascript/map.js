21864c820e125d190a489ff963f8bf02da5eccf4
Ilyaz22 <so.ilia@yandex.ru>
2023-02-14 16:48:45 +0300

    fix map

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,26 +1,29 @@
 ymaps.ready(init);
-var myMap;
 
 function init(){
-  address = document.getElementById('map').getAttribute('data-address');
+  const element = document.getElementById('map')
 
-  myMap = new ymaps.Map("map", {
-      center: [55.76, 37.64],
-      zoom: 10
+  if (!element) return
+
+  const address = element.getAttribute("data-address");
+
+  const myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 10
   });
 
-  myGeocoder = ymaps.geocode(address);
+  const myGeocoder = ymaps.geocode(address);
 
   myGeocoder.then(
     function (res) {
-      coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+      const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
       myMap.geoObjects.add(
-          new ymaps.Placemark(
-            coordinates,
-            {iconContent: address},
-            {preset: 'islands#blueStretchyIcon'}
-          )
+        new ymaps.Placemark(
+          coordinates,
+          {iconContent: address},
+          {preset: 'islands#blueStretchyIcon'}
+        )
       );
 
       myMap.setCenter(coordinates);
